c37066fb82ca8630e8d8f9f7d32092f1178a5ade
happy-everyday-everyweek <18804145766@139.com>
2026-09-15 22:48:37 +0800

    fix(md3): drop absl dependency in quantize/wsmeans (use std::unordered_map)

--- thirdparty/material-color-utilities/cpp/quantize/wsmeans.cc
+++ thirdparty/material-color-utilities/cpp/quantize/wsmeans.cc
@@ -27,7 +27,6 @@
 #include <unordered_set>
 #include <vector>
 
-#include "absl/container/flat_hash_map.h"
 #include "cpp/quantize/lab.h"
 
 constexpr int kMaxIterations = 100;
@@ -64,7 +63,7 @@ QuantizerResult QuantizeWsmeans(const std::vector<Argb>& input_pixels,
   }
 
   uint32_t pixel_count = input_pixels.size();
-  absl::flat_hash_map<Argb, int> pixel_to_count;
+  std::unordered_map<Argb, int> pixel_to_count;
   std::vector<uint32_t> pixels;
   pixels.reserve(pixel_count);
   std::vector<Lab> points;
@@ -75,7 +74,7 @@ QuantizerResult QuantizeWsmeans(const std::vector<Argb>& input_pixels,
     // std::unordered_map 10.2 ms
     // absl::btree_map 9.0 ms
     // absl::flat_hash_map 8.0 ms
-    absl::flat_hash_map<Argb, int>::iterator it = pixel_to_count.find(pixel);
+    std::unordered_map<Argb, int>::iterator it = pixel_to_count.find(pixel);
     if (it != pixel_to_count.end()) {
       it->second++;
 
